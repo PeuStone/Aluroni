dd282341ca99eb49336d21083bded88f8bfe8711
PeuStone <pedrohmotaaa@gmail.com>
2023-08-03 10:18:18 -0300

    Criação de um componente Botão ja estilizado

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,7 +1,7 @@
 import styles from './Cardapio.module.scss';
 
-export default function Cardapio () {
-    return(
+export default function Cardapio() {
+    return (
         <main>
             <nav className="menu">
 
