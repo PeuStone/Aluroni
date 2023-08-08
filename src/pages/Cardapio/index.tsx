bcc4e341bf2aa2d9b2bc03132174e62dd9fa3a1e
PeuStone <pedrohmotaaa@gmail.com>
2023-08-08 10:35:48 -0300

    Adicionando os itens no projeto e seus estilos

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -4,6 +4,7 @@ import styles from './Cardapio.module.scss';
 import { ReactComponent as Logo } from 'assets/logo.svg';
 import Filtros from './Filtros';
 import Ordenador from './Ordenador';
+import Itens from './Itens';
 
 export default function Cardapio() {
     const [busca, setBusca] = useState("");
@@ -35,6 +36,7 @@ export default function Cardapio() {
                         setOrdenador={setOrdenador}
                     />
                 </div>
+                <Itens />
             </section>
         </main>
     )
