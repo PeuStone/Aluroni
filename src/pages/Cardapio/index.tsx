69cc6f72683dd9e6b12dacd12fe6df833bbf9c42
PeuStone <pedrohmotaaa@gmail.com>
2023-08-08 09:41:24 -0300

    Criando e adicionando o ordenador no projeto

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -3,6 +3,7 @@ import Buscador from './Buscador';
 import styles from './Cardapio.module.scss';
 import { ReactComponent as Logo } from 'assets/logo.svg';
 import Filtros from './Filtros';
+import Ordenador from './Ordenador';
 
 export default function Cardapio() {
     const [busca, setBusca] = useState("");
@@ -24,10 +25,11 @@ export default function Cardapio() {
                     setBusca={setBusca}
                 />
                 <div className={styles.cardapio__filtros}>
-                    <Filtros 
-                    filtro={filtro}
-                    setFiltro={setFiltro}
+                    <Filtros
+                        filtro={filtro}
+                        setFiltro={setFiltro}
                     />
+                    <Ordenador />
                 </div>
             </section>
         </main>
